6839bac52f4cfc742ad0a920e88755fabf40441f
sourav <souravkumar59139@gmail.com>
2024-10-27 21:25:47 +0530

    final commit

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -17,15 +17,28 @@ const ContextProvider = (props) => {
     }, 75 * index);
   };
 
+  const newChat = ()=>{
+    setLoading(false)
+    setShowResult(false)
+  }
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    setPrevPrompts((prev) => [...prev, input]);
-    const response = await runChat(input);
+    let response
+    if(prompt !== undefined){
+      response = await runChat(prompt)
+      setRecentPrompt(prompt)
+    }
+    else{
+      setPrevPrompts(prev=>[...prev,input])
+      setRecentPrompt(input);
+      const response = await runChat(input);
+    }
+  
     let responseArray = response.split("**");
-    let newResponse = ""; // Initialize newResponse as an empty string
+    let newResponse = "";
     for (let i = 0; i < responseArray.length; i++) {
       if (i === 0 || i % 2 !== 1) {
         newResponse += responseArray[i];
@@ -54,6 +67,7 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat,
   };
 
   return (
